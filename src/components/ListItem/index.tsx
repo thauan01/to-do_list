ce3ed0629f681cccff3d09ebba9e21599f28916c
Thauan Fernandes <thauan.fernandes@ufpe.br>
2022-06-13 22:39:23 -0300

    Fazendo Projeto 'To-Do List' e Praticando React

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,22 @@
+import * as C from './style';
+import { Item } from '../../types/item';
+//import { isTemplateExpression } from 'typescript';
+import { useState } from 'react';
+type Props = {
+    item: Item
+}
+
+export const ListItem = ({ item }: Props) => {
+    const [isChecked, setIsChecked] = useState(item.done);
+    
+    return (
+        <C.Container done={isChecked}>
+            <input 
+                type="checkbox" 
+                checked={isChecked}
+                onChange={e => setIsChecked(e.target.checked)}
+            />
+            <label>{item.name}</label>
+        </C.Container>
+    )
+}
